a2c3d930347021d75a59e43630023157c9e46a72
Chankso <chanksoitc@gmail.com>
2026-01-15 17:00:19 +0400

    Update astro.config.mjs

--- packages/client/astro.config.mjs
+++ packages/client/astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://chankso.github.io",
-  base: "/origin/",
+  base: "/origin/packages/client",
   output: "static",
 });
